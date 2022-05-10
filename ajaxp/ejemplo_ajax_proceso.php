569b9b26e5de5cac7a4cfd909590fff23063344a
rodrigochigo <100967743+rodrigochigo@users.noreply.github.com>
2022-05-10 14:48:03 -0500

    Update ejemplo_ajax_proceso.php

--- ajaxp/ejemplo_ajax_proceso.php
+++ ajaxp/ejemplo_ajax_proceso.php
@@ -1,4 +1,4 @@
 <?php 
 $resultado = $_POST['valorCaja1'] + $_POST['valorCaja2']; 
-echo $resultados; //haciendo este echo estas respondiendo la solicitud ajax
+echo $resultadoss; //haciendo este echo estas respondiendo la solicitud ajax
 ?>
